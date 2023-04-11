fa983c124ed4fa4400ddfa1a1f14d65ad643ed99
Akit Computer <127582939+akitcomputernigltd@users.noreply.github.com>
2023-04-11 21:39:52 +0100

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($S_POST['submit'])) {
+    $name = trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $subject = trim($_POST['subject']);
+    $message = trim($_POST['message']);
+
+    $myMail = "mymovieisahiyya@gmal.com";
+    $header = "From: " . $email;
+    $message2 = "You have received a mail from " . $name . ". \n\n" . $message;
+
+
+    mail($myMail, $subject, $message2, $header);
+
+    header("Locatoin: index.html?mail sent");
+}
+?>
